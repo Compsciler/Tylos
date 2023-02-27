6573c0dbc8a4cd9f600bed77fa1413890db8e659
Roger Wang <rogerwang2520@gmail.com>
2023-02-27 10:20:02 -0600

    Add MyNetworkManager, spawn points, and ground NavMesh

---
+++ Assets/Scripts/Networking/MyNetworkManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class MyNetworkManager : NetworkManager
+{
+    [SerializeField] GameObject unitSpawnerPrefab;
+
+    NetworkConnectionToClient hostConnection = null;
+
+    public override void OnServerAddPlayer(NetworkConnectionToClient conn)
+    {
+        base.OnServerAddPlayer(conn);
+
+        // TODO: move into StartGame() later
+        // If this is the first player to join, make them the host
+        if (hostConnection == null)
+        {
+            hostConnection = conn;
+        }
+
+        GameObject unitSpawnerInstance = Instantiate(unitSpawnerPrefab, conn.identity.transform.position, conn.identity.transform.rotation);
+        NetworkServer.Spawn(unitSpawnerInstance, conn);
+    }
+}
