1738035faf0121a1e7a6350bbb4f7e942a882ccf
Roger Wang <rogerwang2520@gmail.com>
2023-03-15 17:50:21 -0500

    Add basic unit identity component

---
+++ Assets/Scripts/Unit/UnitIdentity.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitIdentity : MonoBehaviour
+{
+    float r;
+    float g;
+    float b;
+
+    public float R => r;
+    public float G => g;
+    public float B => b;
+}
