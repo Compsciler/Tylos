f7a784af2ad0b9ee34233d03df237e350d8e3b42
eschur2 <eschur2@illinois.edu>
2023-03-29 20:05:11 -0500

    Animations, Lobby
    Animations added: Fades, Title text move to main menu transition
    
    Lobby: Integrated new assets, adjusted playercard prefab (contains a host crown with alpha 0)

--- Assets/Scripts/UI/Title/TitleController.cs
+++ Assets/Scripts/UI/Title/TitleController.cs
@@ -7,14 +7,28 @@ using UnityEngine.SceneManagement;
 public class TitleController : MonoBehaviour
 {
     [SerializeField] private Animator transitionAnimator;
-
+    [SerializeField] private Animator titleTextAnimator;
     [Scene]
     [SerializeField] string mainMenuScene;
 
+    private static readonly int FadeOutTrigger = Animator.StringToHash("FadeOutTrigger");
+    private static readonly int PlayTransition = Animator.StringToHash("PlayTransition");
+
     private IEnumerator LoadSceneWithTransition(string sceneName)
     {
-        transitionAnimator.SetTrigger("FadeOutTrigger");
+        transitionAnimator.SetTrigger(FadeOutTrigger);
         yield return new WaitForSeconds(1f);
+
+        // Set the PlayTransition parameter to true
+        titleTextAnimator.SetBool(PlayTransition, true);
+
+        // Play the title transition animation
+        titleTextAnimator.Play("TitleTextMove");
+
+        // Wait for the title transition animation to complete before loading the next scene
+        // Replace 1f with the duration of your title transition animation
+        yield return new WaitForSeconds(1f);
+
         SceneManager.LoadScene(sceneName);
     }
 
