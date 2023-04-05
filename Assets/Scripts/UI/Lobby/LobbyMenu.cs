4c6823b22937213c5914dc59b94cb8068d6d4203
Roger Wang <rogerwang2520@gmail.com>
2023-04-05 16:44:10 -0500

    Add broken lobby name text display (will fix in next commit)

--- Assets/Scripts/UI/Lobby/LobbyMenu.cs
+++ Assets/Scripts/UI/Lobby/LobbyMenu.cs
@@ -10,18 +10,26 @@ public class LobbyMenu : MonoBehaviour
 {
     // [SerializeField] GameObject lobbyUI;
     [SerializeField] Button startGameButton;
+    [SerializeField] GameObject[] playerCards;
     [SerializeField] TMP_Text[] playerNameTexts;
 
     [SerializeField] LobbyController lobbyController;
 
-    private void Start()
+    string waitingForPlayerText;
+
+    [SerializeField] Color queuedPlayerColor;
+    [SerializeField] Color waitingForPlayerColor;
+
+    void Awake()
     {
+        waitingForPlayerText = playerNameTexts[0].text;
+
         MyNetworkManager.ClientOnConnected += HandleClientConnected;
         MyPlayer.AuthorityOnPartyOwnerStateUpdated += AuthorityHandlePartyOwnerStateUpdated;
         MyPlayer.ClientOnInfoUpdated += ClientHandleInfoUpdated;
     }
 
-    private void OnDestroy()
+    void OnDestroy()
     {
         MyNetworkManager.ClientOnConnected -= HandleClientConnected;
         MyPlayer.AuthorityOnPartyOwnerStateUpdated -= AuthorityHandlePartyOwnerStateUpdated;
@@ -33,21 +41,38 @@ public class LobbyMenu : MonoBehaviour
         // lobbyUI.SetActive(true);
     }
 
+    // TODO: make sure LobbyMenu is DontDestroyOnLoad from MainMenu
     private void ClientHandleInfoUpdated()
     {
         List<MyPlayer> players = ((MyNetworkManager)NetworkManager.singleton).Players;
 
         for (int i = 0; i < players.Count && i < playerNameTexts.Length; i++)
         {
-            playerNameTexts[i].text = players[i].DisplayName;
+            SetPlayerQueued(i, players);
         }
 
         for (int i = players.Count; i < playerNameTexts.Length; i++)
         {
-            playerNameTexts[i].text = "Waiting For Player...";
+            SetPlayerWaiting(i, players);
         }
 
+        Debug.Log($"Player count: {players.Count}");
         startGameButton.interactable = players.Count >= 2;
+    }
+
+    private void SetPlayerQueued(int playerIndex, List<MyPlayer> players)
+    {
+        Debug.Log("here 1");
+        playerNameTexts[playerIndex].text = players[playerIndex].DisplayName;
+        playerCards[playerIndex].GetComponent<RawImage>().color = queuedPlayerColor;
+        Debug.Log("Player " + playerIndex + " is queued");
+    }
+
+    private void SetPlayerWaiting(int playerIndex, List<MyPlayer> players)
+    {
+        playerNameTexts[playerIndex].text = waitingForPlayerText;
+        playerCards[playerIndex].GetComponent<RawImage>().color = waitingForPlayerColor;
+        Debug.Log("Player " + playerIndex + " is waiting");
     }
 
     private void AuthorityHandlePartyOwnerStateUpdated(bool state)
@@ -63,7 +88,7 @@ public class LobbyMenu : MonoBehaviour
     public void LeaveLobby()
     {
         if (NetworkServer.active && NetworkClient.isConnected)
-        {            
+        {
             NetworkManager.singleton.StopHost();
         }
         else
