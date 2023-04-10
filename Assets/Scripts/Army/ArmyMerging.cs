f93a75a4e0be41670722343ee12cb40e376f5115
Roger Wang <rogerwang2520@gmail.com>
2023-04-09 20:55:28 -0500

    Add army merging

---
+++ Assets/Scripts/Army/ArmyMerging.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+[RequireComponent(typeof(Army))]
+public class ArmyMerging : NetworkBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (!isServer) { return; }  // Probably unnecessary, tests say this is never called on the client
+
+        if (!other.TryGetComponent<Army>(out Army otherArmy)) { return; }
+
+        if (!ShouldThisArmyMerge(otherArmy)) { return; }  // Prevents double merging
+
+        MergeWithArmy(otherArmy);
+    }
+
+    private void MergeWithArmy(Army otherArmy)
+    {
+        Army army = GetComponent<Army>();
+
+        army.ArmyUnits.AddRange(otherArmy.ArmyUnits);
+
+        Destroy(otherArmy.gameObject);
+    }
+
+    private bool ShouldThisArmyMerge(Army otherArmy)
+    {
+        Army army = GetComponent<Army>();
+
+        if (army.ArmyUnits.Count != otherArmy.ArmyUnits.Count)
+        {
+            return army.ArmyUnits.Count > otherArmy.ArmyUnits.Count;
+        }
+
+        return gameObject.GetInstanceID() < otherArmy.gameObject.GetInstanceID();
+    }
+}
