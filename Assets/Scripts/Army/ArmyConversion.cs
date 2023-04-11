757131069a6887d6a3420077948f9ddb35e30284
Jacky Park <jackypark9852@gmail.com>
2023-04-11 12:47:32 -0500

    Add conversion coroutine

--- Assets/Scripts/Army/ArmyConversion.cs
+++ Assets/Scripts/Army/ArmyConversion.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using Mirror;
+using UnityEditor;
+using System.Collections;
 
 /// <summary>
 /// 
@@ -13,17 +15,61 @@ public class ArmyConversion : NetworkBehaviour
     [SerializeField]
     AnimationCurve resistanceCurve = new AnimationCurve();
 
+    [SyncVar]
+    private float conversionProgress = 0f;
+    public float ConversionProgress => conversionProgress;
+
     // Resistance to being converted by an enemy army
     // Scales with army size
     [SyncVar]
     private float resistance = 0f;
     public float Resistance => resistance;
 
+    // How long it takes to reset the conversion progress
+    [SerializeField]
+    private float resetTime = 0.5f;
 
+    Coroutine conversionCoroutine = null;
+    private float resetTimer = 0f;
 
     [Server]
     public void SetResistance(int unitCount)
     {
         resistance = resistanceCurve.Evaluate(unitCount / maxUnitCount);
+    }
+
+    [Server]
+    public void Convert(Army otherArmy)
+    {
+        if (conversionCoroutine == null)
+        {
+            StartCoroutine(ConversionCoroutine());
+        }
+
+        if (Mathf.Approximately(conversionProgress, 1f))
+        {
+            // Convert all units
+            otherArmy.ArmyUnits.AddRange(GetComponent<Army>().ArmyUnits);
+            Destroy(gameObject);
+        }
+        else
+        {
+            conversionProgress += Time.deltaTime / resistance;
+        }
+    }
+
+    /// <summary>
+    /// Resets the conversion progress after a certain amount of time
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator ConversionCoroutine()
+    {
+        resetTimer = resetTime;
+        while (resetTimer > 0f)
+        {
+            resetTimer -= Time.deltaTime;
+            yield return null;
+        }
+        conversionProgress = 0f;
     }
 }
